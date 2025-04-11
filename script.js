89b698277d5f1356e7a9feb2653f1873de4fafb2
SHRAMAJEEVI <bankollermanikanta@gmail.com>
2025-04-12 00:21:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-var countDownDate=new Date(" april 15,2025 00:00:00").getTime();
-var x=setInterval(function(){
-  var now=new Date().getTime();
-  var  distance=countDownDate-now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("days").innerHTML=days;
-  document.getElementById("hours").innerHTML=hours;
-  document.getElementById("minutes").innerHTML=minutes;
-  document.getElementById("seconds").innerHTML=seconds;
-  if(distance<0){
-    clearInterval(x);
-    document.getElementById("days").innerHTML=  "00";
-  document.getElementById(" hours").innerHTML= "00";
-  document.getElementById(" minutes").innerHTML= "00";
-  document.getElementById("seconds").innerHTML= "00";
-  }
-},1000);
+let speech=new SpeechSynthesisUtterance();
+ 
+
+document.querySelector("button").addEventListener("click",()=>{
+  speech.text=document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
